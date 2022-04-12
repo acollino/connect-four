77d1edf256478156f42221ceeeb2a593421d1f07
Alex Collino <arcollino@gmail.com>
2022-04-12 11:13:01 -0700

    implement makeBoard

--- connect4.js
+++ connect4.js
@@ -9,7 +9,7 @@ const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-const board = []; // array of rows, each row is array of cells  (board[y][x])
+const board = Array(HEIGHT); // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,6 +17,7 @@ const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  board.fill(Array(WIDTH));
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
